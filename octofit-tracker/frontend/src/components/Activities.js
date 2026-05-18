c1a675d250a58852b9c4479de275fe0cb1a4055f
Shaik Mulla Mahammad Mubarak <shaikmubarak2731@gmail.com>
2026-05-18 08:06:26 +0000

    Create React frontend for OctoFit Tracker

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Activities() {
+  return (
+    <div>
+      <h2>Activities</h2>
+    </div>
+  );
+}
+
+export default Activities;
---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Workouts() {
+  return (
+    <div>
+      <h2>Workouts</h2>
+    </div>
+  );
+}
+
+export default Workouts;
